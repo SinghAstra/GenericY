290e3fdd0c31df1bd9339e34d358698786c4e464
Astra <abhaypratapsinghwd@gmail.com>
2025-10-19 11:07:12 +0530

    Updated Theme

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "@/interfaces/site";
+
+export const siteConfig: SiteConfig = {
+  name: "GenericY",
+  description: "Generic Landing page for any SAAS",
+  keywords: ["Landing Generic Page", "SAAS"],
+  url: "https://genericy.vercel.app",
+  links: {
+    twitter: "https://x.com/XSinghAstra",
+    github: "https://github.com/SinghAstra",
+    githubRepo: "https://github.com/SinghAstra/GenericX",
+  },
+};
